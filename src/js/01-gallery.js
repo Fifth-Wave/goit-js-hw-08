9287d53ce4f18ee581953a568e6325c517091415
Dmytro Yeskov <88843350+Fifth-Wave@users.noreply.github.com>
2022-01-14 19:06:49 +0200

    task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryWraper = document.querySelector('.gallery');
+galleryCreation();
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function galleryCreation() {
+  galleryWraper.insertAdjacentHTML('afterbegin', galleryItems.map(cardCreation).join(''));
+}
+
+function cardCreation({ preview, original, description }) {
+  return ` 
+  
+    <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  
+    `;
+}
