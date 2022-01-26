857c27709dd2b10e1abee09d0976341f462ad9ec
Dmytro Yeskov <wavefifth@gmail.com>
2022-01-26 19:32:24 +0200

    update task no.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+// import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const iframePlayer = new Vimeo.Player(iframe);
+const STORAGE_TIME_KEY = 'videoplayer-current-time';
+
+onPageLoad();
+
+iframePlayer.on('timeupdate', throttle(updateTimeStorage, 1000));
+
+function onPageLoad() {
+  const time = localStorage.getItem(STORAGE_TIME_KEY);
+  if (time) {
+    iframePlayer.setCurrentTime(JSON.parse(time).seconds);
+  }
+}
+
+function updateTimeStorage(currentTime) {
+  localStorage.setItem(STORAGE_TIME_KEY, JSON.stringify(currentTime));
+}
