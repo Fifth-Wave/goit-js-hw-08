24f589dc82bbd0676c6739d92a1dda2b3116d5a3
Fifth-Wave <88843350+Fifth-Wave@users.noreply.github.com>
2022-01-26 21:57:13 +0000

    Deploying to gh-pages from @ Fifth-Wave/goit-js-hw-08@5facc038eb33a066cd4941c133acd7be96a3aeb1 🚀

--- 02-video.df973c86.js
+++ 02-video.df973c86.js
@@ -4,4 +4,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=t(require("lodash.throttle"));function t(e){return e&&e.__esModule?e:{default:e}}const r=document.querySelector("#vimeo-player"),o=new Vimeo.Player(r),n="videoplayer-current-time";function u(){const e=localStorage.getItem(n);e&&o.setCurrentTime(JSON.parse(e).seconds)}function a(e){localStorage.setItem(n,JSON.stringify(e))}u(),o.on("timeupdate",(0,e.default)(a,1e3));
 },{"lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.f413384a.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.df973c86.js.map
